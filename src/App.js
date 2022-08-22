dd3a50717993a0ed18287925b1be8dd08b18f040
Bruno Soares <bruno.sk.8@hotmail.com>
2022-08-22 17:59:25 -0300

    Modified to comport new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Container from "./components/layout/Container";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
+    <div>
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/newproject">Novos Projeto</Link>
+        </div>
+        <Container customClass="min-height">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/company" element={<Company />}></Route>
+            <Route exact path="/contact" element={<Contact />}></Route>
+            <Route exact path="/newproject" element={<NewProject />}></Route>
+          </Routes>
+        </Container>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
