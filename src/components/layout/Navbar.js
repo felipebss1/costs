12d80aa3eb64ffa804d755c10852f2a0b7037dc3
Bruno Soares <bruno.sk.8@hotmail.com>
2022-08-23 12:42:56 -0300

    New Components Created

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import styles from "./Navbar.module.css";
+import logo from "../../img/costs_logo.png";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="Costs" />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/projects">Projetos</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Empresa</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contato</Link>
+          </li>
+        </ul>
+        {/* <Link to="/newproject">Novos Projeto</Link> */}
+      </Container>
+    </nav>
+  );
+}
+
+export default Navbar;
