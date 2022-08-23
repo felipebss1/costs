baaf12825e83045b9bd5c805a2b82e277b874075
Bruno Soares <bruno.sk.8@hotmail.com>
2022-08-23 12:42:21 -0300

    Added New Features

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Container from "./components/layout/Container";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
     <div>
       <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/company">Empresa</Link>
-          <Link to="/contact">Contato</Link>
-          <Link to="/newproject">Novos Projeto</Link>
-        </div>
+        <Navbar />
         <Container customClass="min-height">
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/projects" element={<Projects />}></Route>
             <Route exact path="/company" element={<Company />}></Route>
             <Route exact path="/contact" element={<Contact />}></Route>
             <Route exact path="/newproject" element={<NewProject />}></Route>
           </Routes>
         </Container>
-        <p>Footer</p>
+        <Footer />
       </Router>
     </div>
   );
