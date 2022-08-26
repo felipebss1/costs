682ae0fb05b76a022844677f9800478495fc87bc
Bruno Soares <bruno.sk.8@hotmail.com>
2022-08-26 17:50:57 -0300

    Modified for tests

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,62 @@
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+import styles from "./Projects.module.css";
+import ProjectCard from "../project/ProjectCard";
+import Loading from "../layout/Loading";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
 
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:5000/projects", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProjects(data);
+          setRemoveLoading(true);
+        })
+        .catch((err) => console.log(err))
+    }, 3000);
+  }, []);
+
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              name={project.name}
+              id={project.id}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p>Não projetos cadastrados</p>
+        )}
+      </Container>
     </div>
   );
 }
