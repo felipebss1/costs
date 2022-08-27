a8911628b44a4a7b2be26eb507162ce359deac4f
Bruno Soares <bruno.sk.8@hotmail.com>
2022-08-27 11:12:39 -0300

    Updated to include comp

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import NewProject from "./components/pages/NewProject";
+import Project from "./components/pages/Project";
 import Projects from "./components/pages/Projects";
 
 function App() {
@@ -16,10 +17,11 @@ function App() {
         <Container customClass="min-height">
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
-            <Route exact path="/projects" element={<Projects />}></Route>
-            <Route exact path="/company" element={<Company />}></Route>
-            <Route exact path="/contact" element={<Contact />}></Route>
-            <Route exact path="/newproject" element={<NewProject />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/company" element={<Company />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/newproject" element={<NewProject />}></Route>
+            <Route path="/projects/:id" element={<Project />}></Route>
           </Routes>
         </Container>
         <Footer />
